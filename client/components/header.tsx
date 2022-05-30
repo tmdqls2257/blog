448975caf711859b4e6bb3d0898e1e0478ab5297
Seung Bin Hong <tmdqls2257@gmail.com>
2022-05-30 19:55:52 +0900

    search 컴포넌트를 만들어주었습니다.

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
         {/* 모바일버전에서 클릭하면 메뉴 보이도록 설정하는 것도 한다. (close Mobile Menu)는 다시 버튼 누르면 없어지고 생기고 하도록 한다.  */}
         <div className={styles.container}>
           <div className={styles.logo}></div>
-          <div>
+          <div className={styles.btnContainer}>
             <FontAwesomeIcon
               className={styles.hamburger}
               onClick={barClick}
